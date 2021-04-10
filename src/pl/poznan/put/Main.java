63b7c91306d27066799fcdd3ae2d04469c0b3faa
kamqaa <kamilaacybulska@gmail.com>
2021-04-10 21:54:49 +0200

    pierwszy commit 3

---
+++ src/pl/poznan/put/Main.java
@@ -0,0 +1,14 @@
+package pl.poznan.put;
+
+import edu.princeton.cs.algs4.Graph;
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.StdOut;
+
+public class Main {
+
+    public static void main(String[] args) {
+        In in = new In("");
+        Graph G = new Graph(in);
+        StdOut.println(G);
+    }
+}
