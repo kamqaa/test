7437239ac41e55d2a1b6425abc49197963ff3ba3
Jerzy Krolak <jerzy.krolak@roche.com>
2021-04-10 22:21:46 +0200

    uruchamiamy wszystko

--- src/main/java/pl/poznan/put/Main.java
+++ src/main/java/pl/poznan/put/Main.java
@@ -1,13 +1,12 @@
 package pl.poznan.put;
 
-import edu.princeton.cs.algs4.Graph;
 import edu.princeton.cs.algs4.In;
 import edu.princeton.cs.algs4.StdOut;
 
 public class Main {
 
     public static void main(String[] args) {
-        In in = new In("");
+        In in = new In();
         Graph G = new Graph(in);
         StdOut.println(G);
     }
