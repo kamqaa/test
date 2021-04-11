519cd73d2035e2c0b1b5b7bc5e000d01d36ab0ef
kamqaa <kamilaacybulska@gmail.com>
2021-04-11 12:04:58 +0200

    pierwszy commit 3

--- src/test/java/pl/poznan/put/GraphTest.java
+++ src/test/java/pl/poznan/put/GraphTest.java
@@ -25,4 +25,43 @@ class GraphTest {
                 "8: \n" +
                 "9: \n", G.toString());
     }
+    @Test
+    void testSimple() {
+        In in = new In("proba2.txt");
+
+        Graph G = new Graph(in);
+
+        assertEquals("15 vertices, 4 edges \n" +
+                "0: \n" +
+                "1: 2 \n" +
+                "2: 3 1 \n" +
+                "3: 2 \n" +
+                "4: \n" +
+                "5: \n" +
+                "6: \n" +
+                "7: \n" +
+                "8: \n" +
+                "9: \n" +
+                "10: \n" +
+                "11: \n" +
+                "12: \n" +
+                "13: \n" +
+                "14: \n", G.toString());
+    }
+    @Test
+    void testSimple() {
+        In in = new In("proba3.txt");
+
+        Graph G = new Graph(in);
+
+        assertEquals("8 vertices, 3 edges \n" +
+                "0: \n" +
+                "1: 2 \n" +
+                "2: 3 1 \n" +
+                "3: 2 \n" +
+                "4: \n" +
+                "5: \n" +
+                "6: \n" +
+                "7: \n", G.toString());
+    }
 }
