62a99cd66d29a66b5b2356b2cb380167cdc5d5fa
Jerzy Krolak <jerzy.krolak@roche.com>
2021-04-11 21:24:24 +0200

    zmieniamy testy
    (cherry picked from commit 6ea3c3f70680f22709e1ae2df19f07bfbc1bea53)

--- src/test/java/pl/poznan/put/GraphTest.java
+++ src/test/java/pl/poznan/put/GraphTest.java
@@ -7,23 +7,25 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class GraphTest {
 
+    String separator = System.getProperty("line.separator");
+
     @Test
     void testSimple() {
         In in = new In("simple.txt");
 
         Graph G = new Graph(in);
 
-        assertEquals("10 vertices, 2 edges \n" +
-                "0: \n" +
-                "1: 2 \n" +
-                "2: 3 1 \n" +
-                "3: 2 \n" +
-                "4: \n" +
-                "5: \n" +
-                "6: \n" +
-                "7: \n" +
-                "8: \n" +
-                "9: \n", G.toString());
+        assertEquals("10 vertices, 2 edges " + separator +
+                "0: " + separator +
+                "1: 2 " + separator +
+                "2: 3 1 " + separator +
+                "3: 2 " + separator +
+                "4: " + separator +
+                "5: " + separator +
+                "6: " + separator +
+                "7: " + separator +
+                "8: " + separator +
+                "9: " + separator, G.toString());
     }
     @Test
     void testProba2() {
